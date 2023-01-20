fc06c29309be32c63d2d13e4886b0186a51282f4
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2023-01-20 21:19:33 +0500

    Finish all the required queries

---
+++ .history/data_20230120183525.sql
@@ -0,0 +1,71 @@
+/* Populate database with sample data. */
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', 'Feb 03, 2020', 0, true, '10.23');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', 'Nov 15, 2018', 2, true, '8');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', 'Jan 07, 2021', 1, false, '15.04');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', 'May 12, 2017', 5, true, '11');
+
+/* Query and update animals table */
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Charmander', 'Feb 08, 2020', 0, false, '-11');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Plantmon', 'Nov 15, 2021', 2, true, '-5.7');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Squirtle', 'Apr 02, 1993', 3, false, '-12.13');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Angemon', 'Jun 12, 2005', 1, true, '-45');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Boarmon', 'Jun 07, 2005', 7, true, '20.4');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', 'Oct 13, 1998', 3, true, '17');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Ditto', 'May 14, 2022', 4, true, '22');
+
+/* Insert the following data into the owners table */
+
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age) VALUES ('Jannifer Orwell', 19);
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+
+/* Insert the following data into the species table */
+
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
+
+/* Modify your inserted animals so it includes the species_id value: */
+UPDATE animals
+SET species_id = 2
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species_id = 1
+WHERE species_id IS NULL;
+
+/* Modify your inserted animals to include owner information (owner_id) */
+UPDATE animals
+SET owner_id = 1
+WHERE name = 'Agumon';
+
+UPDATE animals
+SET owner_id = 2
+WHERE name = 'Gabumon' OR name = 'Pikachu';
+
+UPDATE animals
+SET owner_id = 3
+WHERE name = 'Devimon' OR name = 'Plantmon';
+
+UPDATE animals
+SET owner_id = 4
+WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+
+UPDATE animals
+SET owner_id = 5
+WHERE name = 'Angemon' OR name = 'Boarmon';
+
+
+/* Insert the following data for vets */
+
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('William Tatcher', 45, 'Apr 23, 2000');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Maisy Smith', 26, 'Jan 17, 2019');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Stephanie Mendez', 64, 'May 04, 1981');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Jack Harkness', 38, 'Jun 08, 2008');
+
+/* Insert the following data for specialties */
+
---
+++ .history/schema_20230120184514.sql
@@ -0,0 +1,78 @@
+/* Database schema to keep the structure of entire database. */
+
+CREATE TABLE animals (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(100) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	escape_attempts INT NOT NULL,
+	neutered BOOLEAN NOT NULL,
+	weight_kg DECIMAL NOT NULL
+);
+
+/* Query and update animals table */
+
+ALTER TABLE animals ADD species VARCHAR(100);
+
+/* Create a table named owners */
+
+CREATE TABLE owners (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	full_name VARCHAR(100) NOT NULL,
+	age INT)
+
+/* Create a table named species */
+
+CREATE TABLE species (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(100) NOT NULL)
+
+
+/* Modify animals table */
+/* Remove column species */
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+/* Add column species_id which is a foreign key referencing species table */
+
+ALTER TABLE animals
+ADD species_id INT;
+
+ALTER TABLE animals 
+ADD FOREIGN KEY (species_id) REFERENCES species (id)
+
+/* Add column owner_id which is a foreign key referencing the owners table */
+
+ALTER TABLE animals
+ADD owner_id INT;
+
+ALTER TABLE animals 
+ADD FOREIGN KEY (owner_id) REFERENCES owners (id)
+
+/* Create a table named vets */
+
+CREATE TABLE vets (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(100) NOT NULL,
+	age INT,
+	date_of_graduation DATE)
+
+/* Create a "join table" called specializations to handle the relationship. */
+
+CREATE TABLE specializations (
+    species_id INT REFERENCES species (id),
+    vets_id INT REFERENCES vets (id),
+    PRIMARY KEY(species_id,vets_id)
+);
+
+/* Create a "join table" called visits to handle this relationship */
+
+CREATE TABLE visits (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    animals_id INT REFERENCES animals (id),
+    vets_id INT REFERENCES vets (id),
+    date DATE NOT NULL,
+    PRIMARY KEY(id)
+);
+
+
